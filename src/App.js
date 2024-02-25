749e498bc69f67ca4a6be22a60b80d0696b4923e
cyd losekann <cydnel@gmail.com>
2024-02-25 03:09:50 -0300

    comments added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import "./index.css";
 
+// cria a constante 'dorks', dentro da função SearchForm, uma array de strings correspondente a cada dork a ser selecionado
+// cria a constante 'buscas', dentro da função SearchForm, que é um objeto que contém um objeto para cada dork, com as chaves 'termo' (ou seja, o termo a ser pesquisado sob determinado dork) e 'negado' e valores iniciais vazios e falsos, respectivamente
 function SearchForm() {
   const dorks = ['ext:', 'filetype:', 'intext:', 'inurl:', 'cache:'];
   const [buscas, setBuscas] = useState(dorks.reduce((acc, dork) => ({
@@ -8,6 +10,7 @@ function SearchForm() {
     [dork]: { termo: '', negado: false }
   }), {}));
 
+  // constante a ser inserida no input de texto no formulário, que recebe o valor do input e atualiza o estado de 'buscas' com o valor do input
   const handleInputChange = (dork, value) => {
     setBuscas(prevBuscas => ({
       ...prevBuscas,
@@ -15,6 +18,7 @@ function SearchForm() {
     }));
   };
 
+  // faz algo semelhante à constante acima, porém com a checkbox, ou seja, atualiza o estado de 'buscas' com o valor da checkbox; se a checkbox estiver marcada, o valor de 'negado' é true, caso contrário, é false
   const handleCheckboxChange = (dork) => {
     setBuscas(prevBuscas => ({
       ...prevBuscas,
