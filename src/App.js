72b01dfbb3a59105eb9ef38a1eb59633bb16d800
cyd losekann <cydnel@gmail.com>
2024-02-25 01:56:48 -0300

    negado

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
 import React, { useState } from 'react';
-import  "./index.css";
+import "./index.css";
 
 function SearchForm() {
   const dorks = ['ext:', 'filetype:', 'intext:', 'inurl:', 'cache:'];
-  const [buscas, setBuscas] = useState(dorks.reduce((acc, dork) => ({ ...acc, [dork]: '' }), {}));
+  const [buscas, setBuscas] = useState(dorks.reduce((acc, dork) => ({
+    ...acc,
+    [dork]: { termo: '', negado: false }
+  }), {}));
 
   const handleInputChange = (dork, value) => {
-    setBuscas(prevBuscas => ({ ...prevBuscas, [dork]: value }));
+    setBuscas(prevBuscas => ({
+      ...prevBuscas,
+      [dork]: { ...prevBuscas[dork], termo: value }
+    }));
+  };
+
+  const handleCheckboxChange = (dork) => {
+    setBuscas(prevBuscas => ({
+      ...prevBuscas,
+      [dork]: { ...prevBuscas[dork], negado: !prevBuscas[dork].negado }
+    }));
   };
 
   const realizarBusca = () => {
     const termosDeBusca = dorks
-      .filter(dork => buscas[dork])
-      .map(dork => `${dork}${buscas[dork]}`)
+      .filter(dork => buscas[dork].termo)
+      .map(dork => `${buscas[dork].negado ? '-' : ''}${dork}${buscas[dork].termo}`)
       .join(' ');
     const query = `https://www.google.com/search?q=${encodeURIComponent(termosDeBusca)}`;
     window.open(query, '_blank');
@@ -25,9 +38,15 @@ function SearchForm() {
           <label>{dork}</label>
           <input
             type="text"
-            value={buscas[dork]}
+            value={buscas[dork].termo}
             onChange={(e) => handleInputChange(dork, e.target.value)}
           />
+          <input
+            type="checkbox"
+            checked={buscas[dork].negado}
+            onChange={() => handleCheckboxChange(dork)}
+          />
+          <label>Negar</label>
         </div>
       ))}
       <button onClick={realizarBusca}>Buscar</button>
